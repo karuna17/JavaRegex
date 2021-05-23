08dcc27a3ef9813367f856f99902afceae467f98
karuna17 <karunakakade8@gmail.com>
2021-05-23 20:03:31 +0530

    Added: Pin code validation to restrict alphabet

--- DemoRegex.java
+++ DemoRegex.java
@@ -4,25 +4,30 @@ import java.util.regex.Pattern;
 
 public class DemoRegex {
 
-	Scanner sc = new Scanner(System.in);
-	
-	public void validatePin() {
-		System.out.println("Enter your pin code to check");
-		String pin = sc.next();
-		
-		String regex = "^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$";
-		Pattern pinPattern = Pattern.compile(regex);
+	public void validatePin(String pin, Pattern pinPattern) {		
 		Matcher pinMach = pinPattern.matcher(pin);
-		
+
 		if(pinMach.matches()) {
 			System.out.println("Given pin is valid");
 		}else {
 			System.out.println("Given pin is invalid");
 		}		
 	}
-	
+
 	public static void main(String[] args) {
-      DemoRegex d = new DemoRegex();
-      d.validatePin();
+		Scanner sc = new Scanner(System.in);
+		DemoRegex d = new DemoRegex();
+		
+		System.out.println("Enter your pin code to check");
+		String pin = sc.next();
+		String regex = "^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$";
+		Pattern pinPattern = Pattern.compile(regex);
+		d.validatePin(pin, pinPattern);
+		
+		System.out.println("Enter your pin code to restrict alphabet");
+		String pin1 = sc.next();
+		String regex1=",.^'[a-zA-Z]'^[1-9]{1}[0-9]{2}\\\\s{0,1}[0-9]{3}$";
+		Pattern pinPattern1 = Pattern.compile(regex1);
+		d.validatePin(pin1, pinPattern1);
 	}
 }
